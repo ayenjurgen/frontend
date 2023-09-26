d118f5cd136b86bb5dad7c2d2ce983504af60ef5
Greenhills <peligrogreenhills@gmail.com>
2023-09-26 20:07:00 +0800

    Login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 // import { useAuth } from "../contexts/AuthContext";
-import { Button, Form } from "react-bootstrap";
+//import { Button, Form } from "react-bootstrap";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-//   const { setToken, setIsAuthenticated } = useAuth();
+  //   const { setToken, setIsAuthenticated } = useAuth();
 
   const navigate = useNavigate();
 
@@ -28,8 +28,8 @@ const Login = () => {
     const fetchResponse = await fetch(url, requestOptions);
     if (fetchResponse.status === 200) {
       let data = await fetchResponse.json();
-    //   setToken(data.token);
-    //   setIsAuthenticated(true);
+      //   setToken(data.token);
+      //   setIsAuthenticated(true);
       navigate("/");
     } else {
       alert("Invalid Credential");
@@ -38,28 +38,37 @@ const Login = () => {
 
   return (
     <>
-      <h2>Login Page</h2>
-      <Form onSubmit={handleLogin}>
-        <Form.Group className="mb-3 col-md-4">
-          <Form.Label>Email </Form.Label>
-          <Form.Control
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3 col-md-4">
-          <Form.Label>Password </Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+
+      <div className="wrap-div">
+        <div className="xs-pd-20-10 pd-ltr-20">
+          <div class="title pb-20">
+            <h2 class="h3 mb-0">Login</h2>
+          </div>
+          <form onSubmit={handleLogin}>
+            <div className="card card-box" id="bastaCard">
+              <div className="card-body">
+                <div className="form-group">
+                  <label class="col-form-label" style={{marginLeft: 20}}>Email</label>
+                  <div class="col">
+                    <input class="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label class="col-form-label" style={{marginLeft: 20}}>Password</label>
+                  <div class="col">
+                    <input class="form-control" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                </div>
+              </div>
+              <div className="card-body">
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,7 +1,30 @@
 <>
 
-
   <>
+  <>
+      <h2>Login Page</h2>
+      <Form onSubmit={handleLogin}>
+        <Form.Group className="mb-3 col-md-4">
+          <Form.Label>Email </Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 col-md-4">
+          <Form.Label>Password </Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </>
 
     <h1>Product Create</h1>
     <form onSubmit={handleSubmit}>
